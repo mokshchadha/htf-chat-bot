fee90d6c9f72686feb08fab9a21e738ae1238a15
moksh <moksh@pop-os.localdomain>
2021-10-24 10:36:24 +0530

    chat bot switch case added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").Server(app);
 
-const io = require("socket.io")(server, {cors:{origin:"*"}});
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 const next = require("next");
 
@@ -12,17 +12,41 @@ const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
 let port = 3000;
+let questionIndex = 0;
 
 function chatBot(query) {
-  return "this is chat bot response w regards to "+ query;
+  if (questionIndex == 0) {
+    questionIndex += 1;
+    return "hello freind! How are you ?";
+  } else if (questionIndex == 1) {
+    questionIndex += 1;
+    return "where are you from?";
+  }
+  else if(questionIndex==2)
+   {
+       questionIndex+=1;
+       return 'Would you like to learn about cyber bullying?'
+
+   }
+   else if(questionIndex==3 && query.toLowerCase().includes("no")) {
+       questionIndex++;
+       return 'ok nice meeting you :)'
+   }
+   else if(questionIndex==3 && query.toLowerCase().includes("yes")) {
+    questionIndex++;
+    return 'lets get going'
+}
+
+return "wait for sometime i am thinking ."
+   
 }
 
 io.on("connect", (socket) => {
-    socket.emit("now", { message: "welcome Lets start chatting?" });
-    socket.on("user-query-text", (data) => {
-    console.log(socket.id)
+  socket.emit("now", { message: "welcome Lets start chatting?" });
+  socket.on("user-query-text", (data) => {
+    console.log(socket.id);
     const response = chatBot(data.text);
-    console.log(response)
+    console.log(response);
     socket.emit("chat-bot-reply", { text: response });
   });
 });
